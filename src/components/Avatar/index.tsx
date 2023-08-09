52b2d8de4c7bf77072176084a4457255d457bddf
Eduardo <eduardobrasil2706@gmail.com>
2023-08-09 17:06:38 -0300

    estilizando a sidedbar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/EduardoRDSR.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/EduardoRDSR.png" />
-    <Titulo fontSize={20}>Eduardo Rodrigues</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Eduardo Rodrigues</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        EduardoRDRS
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Desenvolvedor Front-end | WEB
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
