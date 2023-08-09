e6c87011db7038060bdecde5fa7830172593daa4
Eduardo <eduardobrasil2706@gmail.com>
2023-08-09 14:07:48 -0300

    construindo a estrutura e a seção "sobre mim"

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/EduardoRDSR.png" />
+    <Titulo fontSize={20}>Eduardo Rodrigues</Titulo>
+  </aside>
+)
+
+export default Sidebar
